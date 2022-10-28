167ed30dbe38b5b83a40084655b5e284cdae1a9c
STEpHANE <95184279+ZFEbHVUE@users.noreply.github.com>
2022-10-29 00:07:19 +0200

    Update BUILD_15KHZ_BATOCERA.sh

--- userdata/system/BUILD_15KHz/Batocera_ALLINONE/BUILD_15KHZ_BATOCERA.sh
+++ userdata/system/BUILD_15KHz/Batocera_ALLINONE/BUILD_15KHZ_BATOCERA.sh
@@ -924,11 +924,11 @@ case $Version_of_batocera in
 		sed -e "s/\[monitor-name\]/$monitor_name/g" -e "s/\[super_width\]/$super_width/g" -e "s/\[dotclock_min_value\]/$dotclock_min/g"  /userdata/system/BUILD_15KHz/etc_configs/switchres.ini-generic-v36 > /etc/switchres.ini
 		chmod 755 /etc/switchres.ini
 
-			# correction from V36 (dmanlfc)
-			cp /userdata/system/BUILD_15KHz/UsrBin_configs/Nvidia/batocera-nvidia.patch /usr/bin/batocera-nvidia
-			chmod 755 /usr/bin/batocera-nvidia
-			cp /userdata/system/BUILD_15KHz/etc_configs/Nvidia/S05nvidia.patch  /etc/init.d/S05nvidia
-			chmod 755 /etc/init.d/S05nvidia
+#			# correction from V36 (dmanlfc)
+#			cp /userdata/system/BUILD_15KHz/UsrBin_configs/Nvidia/batocera-nvidia.patch /usr/bin/batocera-nvidia
+#			chmod 755 /usr/bin/batocera-nvidia
+#			cp /userdata/system/BUILD_15KHz/etc_configs/Nvidia/S05nvidia.patch  /etc/init.d/S05nvidia
+#			chmod 755 /etc/init.d/S05nvidia
 
 
 
