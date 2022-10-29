f1121d40a45de03b687db5f495eaab84b881909e
Rion <rion198376@gmail.com>
2022-10-29 22:18:45 +0200

    Better descriptions for choosing video drivers
    Better descriptions for choosing video drivers for AMD.
    No more confusion for the end user.

--- userdata/system/BUILD_15KHz/Batocera_ALLINONE/BUILD_15KHZ_BATOCERA.sh
+++ userdata/system/BUILD_15KHz/Batocera_ALLINONE/BUILD_15KHZ_BATOCERA.sh
@@ -522,9 +522,12 @@ clear
 if [ "$TYPE_OF_CARD" == "AMD/ATI" ]; then
         
 	echo "#######################################################################"
-	echo "##		Which kind of drivers you want to use ?              ##"
-	echo "##		If you get a black screen after reboot               ##"
-	echo "##              then choose another driver                           ##"
+	echo "##               Which kind of drivers do you want to use ?          ##"
+        echo "##                                                                   ##"
+        echo "##            Please note, RX and R cards older than the R7 240      ##"
+        echo "##            just do not support the amdgpu drivers, period, and    ##"
+        echo "##            doing this while using them will result in a           ##"
+        echo "##                       black screen after reboot.                  ##"
 	echo "#######################################################################"
 
         declare -a driver_ATI=( "AMDGPU" "RADEON" )
